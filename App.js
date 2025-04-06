1ad8f4071b162760f67a5e78be0b8aa83af185da
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-04-06 12:50:15 +0900

    style: React Native 스타일 객체 다루기

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text onPress={() => alert("Hello World")}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Another piece of text</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello, world!</Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -18,5 +18,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: "blue",
   },
 });
