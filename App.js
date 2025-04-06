463c58dd39a74a6460c9403b19bfea44318d266d
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-04-06 13:25:33 +0900

    feat: safe area provider 적용 및 레이아웃 개선

--- App.js
+++ App.js
@@ -1,44 +1,52 @@
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  View,
-  Button,
-  TextInput,
-} from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View style={styles.appContainer}>
-        <View style={styles.inputContainer}>
-          <TextInput style={styles.textInput} placeholder="Yor course goal!" />
-          <Button title="Add Goal" />
+    <SafeAreaProvider>
+      <SafeAreaView edges={["top", "bottom"]} style={{ flex: 1 }}>
+        <View style={styles.appContainer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Yor course goal!"
+            />
+            <Button title="Add Goal" />
+          </View>
+          <View style={styles.goalsContainer}>
+            <Text>List of Goals</Text>
+          </View>
         </View>
-        <View>
-          <Text>List of Goals</Text>
-        </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingHorizontal: 16,
   },
 
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
 
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+
+  goalsContainer: {
+    flex: 4,
   },
 });
