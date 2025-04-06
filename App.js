26b39944237bdcb768d751984cf611cf4d507949
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-04-06 13:51:32 +0900

    feat: 리스트 스크롤뷰 적용

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
@@ -32,9 +39,13 @@ export default function App() {
             <Button title="Add Goal" onPress={addGoalHandler} />
           </View>
           <View style={styles.goalsContainer}>
-            {courseGoals.map((goal) => (
-              <Text key={goal}>{goal}</Text>
-            ))}
+            <ScrollView>
+              {courseGoals.map((goal) => (
+                <Text style={styles.goalItem} key={goal}>
+                  {goal}
+                </Text>
+              ))}
+            </ScrollView>
           </View>
         </View>
       </SafeAreaView>
@@ -67,6 +78,14 @@ const styles = StyleSheet.create({
   },
 
   goalsContainer: {
-    flex: 4,
+    flex: 5,
+  },
+
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
   },
 });
