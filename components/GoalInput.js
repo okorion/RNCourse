2e80dcf056f309548d5facba3d001859e6cf1a53
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-04-07 21:54:03 +0900

    chore: 이미지 추가 및 적용

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Image,
+} from "react-native";
 
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -16,6 +23,11 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image
+          source={require("../assets/images/goal.png")}
+          style={styles.image}
+          resizeMode="contain"
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Yor course goal!"
@@ -42,10 +54,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    backgroundColor: "#311b6b",
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
 
   textInput: {
