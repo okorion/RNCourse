acc1f6b34f2b806cab813efa982f18c00f6573b9
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-04-13 16:30:27 +0900

    style: iOS, Android 버튼 스타일 적용

--- apps/MiniGame/screens/StartGameScreen.js
+++ apps/MiniGame/screens/StartGameScreen.js
@@ -1,19 +1,10 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput
-        placeholder="Enter a number"
-        style={{
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 10,
-          paddingHorizontal: 10,
-        }}
-      />
+    <View style={styles.inputContainer}>
+      <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -21,3 +12,18 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4, // Android shadow
+    shadowColor: "black", // iOS shadow
+    shadowOffset: { width: 0, height: 2 }, // iOS shadow
+    shadowRadius: 6, // iOS shadow
+    shadowOpacity: 0.25, // iOS shadow
+  },
+});
