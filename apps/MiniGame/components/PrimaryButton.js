ad29c7f1e12ba4a7f26880f94b720ae823b89961
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-04-13 16:45:01 +0900

    style: 버튼 효과 추가

--- apps/MiniGame/components/PrimaryButton.js
+++ apps/MiniGame/components/PrimaryButton.js
@@ -1,11 +1,52 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Pressed!");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 28,
+    margin: 4,
+    elevation: 2, // Android shadow
+    shadowColor: "black", // iOS shadow
+    shadowOffset: { width: 0, height: 2 }, // iOS shadow
+    shadowRadius: 6, // iOS shadow
+    shadowOpacity: 0.25, // iOS shadow
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
