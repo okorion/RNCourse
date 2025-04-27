a8955bed9402b42920774b2f62b6144acc3f0ad5
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-04-27 18:23:11 +0900

    feat: 음식 별 정보 렌더

--- apps/FoodNavi/screens/MealsOverviewScreen.js
+++ apps/FoodNavi/screens/MealsOverviewScreen.js
@@ -1,13 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.includes(catId);
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={style.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
